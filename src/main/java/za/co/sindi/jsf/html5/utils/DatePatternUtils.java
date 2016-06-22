4483133041a7fd1f0e6fc9d8e7046f454a773b72
Buhake Sindi <buhake.sindi@sgits.co.za>
2016-06-22 11:31:03 +0200

    Code Refactoring...

--- src/main/java/za/co/sindi/jsf/html5/utils/DatePatternUtils.java
+++ src/main/java/za/co/sindi/jsf/html5/utils/DatePatternUtils.java
@@ -5,6 +5,7 @@ package za.co.sindi.jsf.html5.utils;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,31 +30,30 @@ public final class DatePatternUtils {
 	private static final String TIME_PATTERN = "([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])";
 //	private static final String PARTIAL_TIME_PATTERN = TIME_PATTERN + "(\\.\\d+)";
 
+	private static final Map<String, String> DATE_FORMAT_PATTERN;
 	
-	private static final Map<String, String> DATE_FORMAT_PATTERN = new HashMap<String, String>() {/**
-		 * 
-		 */
-		private static final long serialVersionUID = 5555766111705777401L;
-
-	{
+	static {
+		Map<String, String> dateFormatPattern = new HashMap<String, String>();
 		//Date
-		put("^" + DATE_PATTERN + "$", "yyy-MM-dd");
+		dateFormatPattern.put("^" + DATE_PATTERN + "$", "yyy-MM-dd");
 		
 		//Datetime
-		put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss");
-		put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d)$", "yyyy-MM-dd'T'HH:mm:ss.S");
-		put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d{2})$", "yyyy-MM-dd'T'HH:mm:ss.SS");
-		put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d{3})$", "yyyy-MM-dd'T'HH:mm:ss.SSS");
-		put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + Z_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss'Z'");
-		put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d)" + Z_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss.S'Z'");
-		put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d{2})" + Z_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss.SS'Z'");
-		put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d{3})" + Z_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");		
-		put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + TIME_NUMOFFSET_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ssZ");
+		dateFormatPattern.put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss");
+		dateFormatPattern.put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d)$", "yyyy-MM-dd'T'HH:mm:ss.S");
+		dateFormatPattern.put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d{2})$", "yyyy-MM-dd'T'HH:mm:ss.SS");
+		dateFormatPattern.put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d{3})$", "yyyy-MM-dd'T'HH:mm:ss.SSS");
+		dateFormatPattern.put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + Z_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss'Z'");
+		dateFormatPattern.put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d)" + Z_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss.S'Z'");
+		dateFormatPattern.put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d{2})" + Z_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss.SS'Z'");
+		dateFormatPattern.put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d{3})" + Z_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");		
+		dateFormatPattern.put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + TIME_NUMOFFSET_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ssZ");
 		//Disabling as we are using XMLGregorianCalendar.
-//		put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d)" + TIME_NUMOFFSET_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss.SZ");
-//		put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d{2})" + TIME_NUMOFFSET_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss.SSZ");
-//		put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d{3})" + TIME_NUMOFFSET_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-	}};
+//		dateFormatPattern.put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d)" + TIME_NUMOFFSET_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss.SZ");
+//		dateFormatPattern.put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d{2})" + TIME_NUMOFFSET_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss.SSZ");
+//		dateFormatPattern.put("^" + DATE_PATTERN + T_PATTERN + TIME_PATTERN + "(\\.\\d{3})" + TIME_NUMOFFSET_PATTERN + "$", "yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+		
+		DATE_FORMAT_PATTERN = Collections.unmodifiableMap(dateFormatPattern);
+	}
 	
 	/**
 	 * 
